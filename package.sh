636e2a00b9766b0befc3538c706de31dec3bb671
Guillaume Naimi <guillaume.naimi@saagie.com>
2018-07-09 16:28:39 +0200

    feat: Finalize automatisation script

--- package.sh
+++ package.sh
@@ -7,9 +7,17 @@ for d in */ ; do
         helm package $d --destination appstore-tgz
     fi
 done
-
-git checkout gh-pages
+commit=$(git log -1 --pretty=%B)
+sha1=$(git rev-parse HEAD)
+echo $commit
+message=$(git checkout gh-pages)
+if [  $? -ne 0 ] ; then
+    exit 1
+fi
 mv appstore-tgz/* .
 helm repo index .
+git add .
+git commit -m "packaging : $commit ($sha1)"
+git push
 
 
